651ebf51cb582a5ebf56dd9615523c2822bbe386
Yuri Chukhlib <Developer4@MacBook-Pro-Yurii.local>
2017-10-03 09:04:03 +0300

    Test
    Test

---
+++ RXSwift/RXSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RXSwift
+//
+//  Created by Yurii Chukhlib on 12.09.2017.
+//  Copyright © 2017 D4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
